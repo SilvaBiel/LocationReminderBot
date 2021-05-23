abe56f51ef38f59e5bcb2c384e669f60993a5c80
Andrew Golubev <golubevcg@gmail.com>
2021-05-23 12:13:40 +0300

    Initial commit

---
+++ model/entity/Task.py
@@ -0,0 +1,9 @@
+id
+header
+body
+location  (hashed)
+radius
+date  (hashed)
+time  (hashed)
+state (completed\deleted\in work)
+user user_id
---
+++ model/entity/User.py
@@ -0,0 +1,12 @@
+# id
+# login (hashed)
+# password (hashed)
+# tasks_list (ids)
+# geo_request_rate (1 min 5 min 10 min 15 min 30 min 60 min)
+
+class User:
+    id =
+    login = 
+    password = 
+    tasks_list = 
+    geo_request_rate = 
